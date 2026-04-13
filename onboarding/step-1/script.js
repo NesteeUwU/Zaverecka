191d27897d31367cd11a2f0ac5861aee3fa9decf
NesteeUwU <nestee.uwu@gmail.com>
2026-04-13 11:46:18 +0200

    connected step 1 with step 2 (js)

---
+++ onboarding/step-1/script.js
@@ -0,0 +1,6 @@
+const cards = document.getElementById('german');
+cards.addEventListener("click",selected)
+function selected(){
+    german.classList.toggle('selected')
+    button.classList.toggle('enabled')
+};
