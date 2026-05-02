b1b0f9be66b278262cc5dec8af7e35ac7db8e552
NesteeUwU <nestee.uwu@gmail.com>
2026-05-02 18:28:36 +0200

    made hamburger menu work, polishing next

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let colorScheme = document.querySelector('#theme-toggle');
 let divider = document.querySelector('#lightmode-divider');
 let body = document.querySelector('body');
+let hamburger = document.querySelector('.hamburger');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+let responsivity = document.querySelector('.responsivity');
+let nav = document.querySelector('nav');
 
 colorScheme.addEventListener('click', function() {
     body.classList.toggle('light-mode');
@@ -9,5 +13,18 @@ colorScheme.addEventListener('click', function() {
         divider.classList.add('hidden');
     } else {
         divider.classList.remove('hidden');
+    }
+});
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle('change');
+    if (hamburger.classList.contains('change')) {
+        hamburgerMenu.classList.add('active');
+        responsivity.classList.add('vertical');
+        nav.classList.add('vertical');
+    }
+    else{
+        hamburgerMenu.classList.remove('active');
+        responsivity.classList.remove('vertical');
+        nav.classList.remove('vertical');
     }
 });
